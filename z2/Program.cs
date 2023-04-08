32b5340892dbdac80c1b4bc244f6de3555187d07
Rashid <rashidberetar@gmail.com>
2023-04-08 18:03:57 +0300

    Правки

--- z2/Program.cs
+++ z2/Program.cs
@@ -1,5 +1,4 @@
 ﻿Console.WriteLine("Введите числа");
 int a1 = int.Parse(Console.ReadLine());
 int a2 = int.Parse(Console.ReadLine());
-// Console.WriteLine(${max = Math.Max(a1, a2), min = Math.Min(a1, a2)}); - почему-то не работает
 Console.WriteLine("max = " + Math.Max(a1, a2) + ", min = " + Math.Min(a1, a2));
