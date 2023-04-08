1b2d5bca22ab45355cba9fcda0575524339b361c
Rashid <rashidberetar@gmail.com>
2023-04-08 18:24:27 +0300

    Изменил z8 для себя. Чтобы было понятнее решение

--- z8/Program.cs
+++ z8/Program.cs
@@ -2,7 +2,7 @@
 int N = int.Parse(Console.ReadLine());
 for (int i = 2; i <= N; i += 2)
 {
-    if (i < N - 1)
+    if (i <= N - 2)
         Console.Write($"{i}, ");
     else
         Console.Write(i);
