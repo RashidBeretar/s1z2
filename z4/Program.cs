aca1884e390e2237420e7e3acb76ce9cd616fdc3
Rashid <rashidberetar@gmail.com>
2023-04-08 16:33:16 +0300

    Добавляем папку четвёртого задания

---
+++ z4/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите числа");
+int a1 = int.Parse(Console.ReadLine());
+int a2 = int.Parse(Console.ReadLine());
+int a3 = int.Parse(Console.ReadLine());
+Console.WriteLine("Максимальное число " + Math.Max(a1, a2, a3));
