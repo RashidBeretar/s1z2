557db47c5490753431e70226b39e719956516c10
Rashid <rashidberetar@gmail.com>
2023-04-08 17:28:50 +0300

    Добавили решение z8

--- z8/Program.cs
+++ z8/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.WriteLine("Введите число");
 int N = int.Parse(Console.ReadLine());
-for (int i = 0; i <= N; i++)
+for (int i = 2; i <= N; i += 2)
 {
-    if (i % 2 == 0)
-    {
+    if (i < N - 1)
         Console.Write($"{i}, ");
-    }
+    else
+         Console.Write($"{i}.");
 }
